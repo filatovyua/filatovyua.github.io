7a3f642d4938f02eda126e206f2dab8bdb7b0855
Юрий <yuriiall@mail.ru>
2018-06-20 17:35:17 +0300

    start

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,7 @@
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+
+firebase.initializeApp({
+  messagingSenderId: '958975194254'
+});
+
